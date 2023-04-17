32b90426fea86a9aead383d6569b03aa49c87371
Kanghyejin <rkdgpwlsz@naver.com>
2023-04-17 17:53:01 +0900

    로그인 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -12,5 +13,16 @@ export class AuthService {
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.userRepository.findOne({where: {username}});
+
+        if(user && (await bcrypt.compare(password, user.password))){
+            return "signin seccess"
+        } else {
+            return "signin failed"
+        }
     }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,8 @@ import { DataSource, Repository } from "typeorm";
 import { User } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credential.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import * as bcrypt from 'bcryptjs';
 
 export class UserRepository extends Repository<User> {
     constructor(@InjectRepository(User) private dataSource: DataSource) {
@@ -10,9 +12,21 @@ export class UserRepository extends Repository<User> {
 
     async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void>{
         const { username, password } = authCredentialsDto;
-        const user = this.create({ username, password });
 
-        await this.save(user);
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const user = this.create({ username, password: hashedPassword});
+
+        try {
+            await this.save(user);
+        } catch (error) {
+            if (error.code === '23505') {
+                throw new ConflictException('Existing username');
+            } else {
+                throw new InternalServerErrorException();
+            }
+        }
     }
 
     // async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
