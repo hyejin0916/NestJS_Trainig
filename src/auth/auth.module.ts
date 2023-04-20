922d11a0558c4823174e34ce457d0872906a9758
Kanghyejin <rkdgpwlsz@naver.com>
2023-04-20 09:31:18 +0900

    설정 및 마무리 완료2

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -7,15 +7,18 @@ import { User } from './user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     // 모듈 등록
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'Secret1234',
+      secret: jwtConfig.secret,
       signOptions: {
-        expiresIn: 60 * 60,
+        expiresIn: process.env.JWT_SECRET || jwtConfig.expiresIn,
       }
     }),
     TypeOrmModule.forFeature([User])
