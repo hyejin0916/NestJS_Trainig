9b7063b59ed47fc688d303e3f50da71e87373853
Kanghyejin <rkdgpwlsz@naver.com>
2023-04-20 09:15:35 +0900

    설정 1차

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
-  const port = 3000;
-  await app.listen(3000);
+  const serverConfig = config.get('server')
+  // config의 server영역을 가지고 오겠다.
+
+  // const port = 3000;
+  const port = serverConfig.port;
+  await app.listen(port);
   logger.log(`Application running on port ${port}`)
 }
 bootstrap();
