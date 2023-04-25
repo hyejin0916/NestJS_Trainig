1f764de83e37ff6e6e3b3cd19fdc1944679cdec8
Kanghyejin <rkdgpwlsz@naver.com>
2023-04-25 14:58:38 +0900

    Swagger 적용

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
-import * as config from 'config';
+import * as user_config from 'config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const logger = new Logger();
   const app = await NestFactory.create(AppModule);
 
-  const serverConfig = config.get('server')
+  const serverConfig = user_config.get('server')
   // config의 server영역을 가지고 오겠다.
+
+  const config = new DocumentBuilder()
+    .setTitle('Board example')
+    .setDescription('The board API description')
+    .setVersion('1.0')
+    .addTag('Board')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   // const port = 3000;
   const port = serverConfig.port;
